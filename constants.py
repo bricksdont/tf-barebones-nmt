a1d7eeeb13ae40443666216f7af28959a70bfb01
Mathias Müller <mathias.mueller@uzh.ch>
2018-02-06 17:46:45 +0100

    init

---
+++ constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Authors:
+# Matvey Ezhov / https://github.com/ematvey
+# Mathias Müller / mmueller@cl.uzh.ch
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+PAD = 0
+EOS = 1
